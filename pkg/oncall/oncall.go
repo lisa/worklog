fc0138ad2bac9595610cc0b4873cd59a4001f498
Lisa Seelye <lisa@users.noreply.github.com>
2020-07-24 09:22:33 -0400

    reorg and add on-call util
    Signed-off-by: Lisa Seelye <lisa@users.noreply.github.com>

---
+++ pkg/oncall/oncall.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+)
+
+const dayTemplate string = `= %s =
+
+== Handovers ==
+
+=== %s AM Handover ===
+
+=== %s PM Handover ===
+
+== %s Pages ==
+
+`
+
+func monthToShort(m time.Month) string {
+	switch m {
+	case time.January:
+		return "Jan"
+	case time.February:
+		return "Feb"
+	case time.March:
+		return "Mar"
+	case time.April:
+		return "Apr"
+	case time.May:
+		return "May"
+	case time.June:
+		return "Jun"
+	case time.July:
+		return "Jul"
+	case time.August:
+		return "Aug"
+	case time.September:
+		return "Sep"
+	case time.October:
+		return "Oct"
+	case time.November:
+		return "Nov"
+	case time.December:
+		return "Dec"
+	}
+	return "Unknown"
+}
+
+func printCategories(day int, month time.Month, year int, kind string) {
+
+	fmt.Printf("[[Category:On-Call]]\n[[Category:On-Call/%d]]\n[[Category:On-Call/%d/%s]]\n",
+		year, year, monthToShort(month))
+	end := time.Date(year, month, day+7, 8, 0, 0, 0, time.Now().Location())
+	if end.Month() > month {
+		fmt.Printf("[[Category:On-Call/%d/%s]]\n", end.Year(), monthToShort(end.Month()))
+	}
+	fmt.Printf("[[Category:On-Call/%s]]\n", kind)
+}
+
+func printDay(day string) {
+	fmt.Printf(dayTemplate, day, day, day, day)
+}
+
+func main() {
+	currentYear := flag.Int("cy", time.Now().Year(), "Current year")
+	currentMonth := flag.Int("cm", int(time.Now().Month()), "Current oncall's month (eg 1-12)")
+	currentDay := flag.Int("cd", time.Now().Day(), "Current workday's day (eg 1-31)")
+	onCallType := flag.String("type", "primary", "On-Call Type (Primary/Secondary/Weekend)")
+	flag.Parse()
+
+	today := time.Date(*currentYear, time.Month(*currentMonth), *currentDay, 8, 0, 0, 0, time.Now().Location())
+
+	// On-call day order
+	days := []string{"Friday", "Monday", "Tuesday", "Wednesday", "Thursday"}
+
+	printCategories(today.Day(), today.Month(), today.Year(), *onCallType)
+
+	for _, day := range days {
+		printDay(day)
+	}
+}
